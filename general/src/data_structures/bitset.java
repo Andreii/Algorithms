b19444db5bf48c5ce9aa25e392e017a9dc49288d
Andreii <andrei@omtia.com>
2021-03-15 11:48:02 +0100

    Bitsets

---
+++ general/src/data_structures/bitset.java
@@ -0,0 +1,32 @@
+package data_structures;
+
+import java.util.BitSet;
+import java.util.stream.IntStream;
+
+public class bitset {
+    public static final int N = 100;
+
+    public static void main(String[] args) {
+
+        BitSet bitset = new BitSet(N);
+
+        bitset.set(3);
+        bitset.set(4);
+        bitset.set(5);
+
+        printBitset(bitset);
+        bitset = bitset.get(1, bitset.length());
+        printBitset(bitset);
+        System.out.println(bitset.cardinality());
+    }
+
+    private static void printBitset(BitSet bitSet) {
+        System.out.println(IntStream.range(0, N).mapToObj(i -> bitSet.get(N - i - 1) ? "1" : "0").collect(
+                () -> new StringBuilder(N),
+                StringBuilder::append,
+                StringBuilder::append
+
+                // cristi 13 martie nascut
+        ).toString() + " " + bitSet.cardinality());
+    }
+}
