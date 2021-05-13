62a481451aa4ea9e5a1bf2030717aafaa6e9cb6f
Andreii <andrei@omtia.com>
2021-05-13 21:16:41 +0200

    Karatsuba improvement

--- general/src/algorithms/Karatsuba.java
+++ general/src/algorithms/Karatsuba.java
@@ -8,14 +8,17 @@ public class Karatsuba {
         BigInteger nr1 = new BigInteger("3141592653589793238462643383279502884197169399375105820974944592");
         BigInteger nr2 = new BigInteger("2718281828459045235360287471352662497757247093699959574966967627");
 
-        System.out.printf("Result is %d", karatsuba(nr1,nr2, 64));
+
+        System.out.printf("Result is %d", karatsuba(nr1,nr2));
     }
 
-    private static BigInteger karatsuba(BigInteger number1, BigInteger number2, int length) {
+    private static BigInteger karatsuba(BigInteger number1, BigInteger number2) {
+        int length = Math.min(number1.toString().length(), number2.toString().length());
+        int half = (int) Math.floor(length/2);
+
         if(length == 1) {
             return number1.multiply(number2);
         } else {
-            int half = length / 2;
             // a,b = first and second half of number1
             // c,d = first and second half of number2
             // p = a + b,  q = c + d
@@ -23,15 +26,17 @@ public class Karatsuba {
             // adbc = pq - ac - bd
             // result = 10^2 * ac + 10^n/2 * adbc + bd
             BigInteger a = new BigInteger(number1.toString().substring(0,half));
-            BigInteger b = new BigInteger((number1.toString().substring(half)).equals("") ? "0" : number1.toString().substring(half) );
+            BigInteger b = new BigInteger(number1.toString().substring(half));
+
             BigInteger c = new BigInteger(number2.toString().substring(0,half));
-            BigInteger d = new BigInteger((number2.toString().substring(half)).equals("") ? "0" : number2.toString().substring(half) );
-            BigInteger ac = karatsuba(a, c, half);
-            BigInteger bd = karatsuba(b, d, half);
-            BigInteger pq = karatsuba(a.add(b), c.add(d), half);
+            BigInteger d = new BigInteger(number2.toString().substring(half));
+
+            BigInteger ac = karatsuba(a, c);
+            BigInteger bd = karatsuba(b, d);
+            BigInteger pq = karatsuba(a.add(b), c.add(d));
             BigInteger adbc = pq.subtract(ac).subtract(bd);
 
-            return new BigInteger(String.valueOf((int)Math.pow(10, length)))
+            return new BigInteger(String.valueOf((int)Math.pow(10, half*2)))
                     .multiply(ac)
                     .add(new BigInteger(String.valueOf((int)Math.pow(10, half))).multiply(adbc))
                     .add(bd);
