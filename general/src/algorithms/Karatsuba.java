0d57b90dac59052e3fa436575b783387b3ccce7d
Andreii <andrei@omtia.com>
2021-05-13 21:42:55 +0200

    Karatsuba finalize

--- general/src/algorithms/Karatsuba.java
+++ general/src/algorithms/Karatsuba.java
@@ -9,14 +9,14 @@ public class Karatsuba {
         BigInteger nr2 = new BigInteger("2718281828459045235360287471352662497757247093699959574966967627");
 
 
-        System.out.printf("Result is %d", karatsuba(nr1,nr2));
+        System.out.printf("Result is %s", karatsuba(nr1,nr2).toString());
     }
 
     private static BigInteger karatsuba(BigInteger number1, BigInteger number2) {
         int length = Math.min(number1.toString().length(), number2.toString().length());
         int half = (int) Math.floor(length/2);
 
-        if(length == 1) {
+        if(length <= 2) {
             return number1.multiply(number2);
         } else {
             // a,b = first and second half of number1
@@ -25,21 +25,18 @@ public class Karatsuba {
             // recursive ac = a * c, bd = b * d, pq = p * q
             // adbc = pq - ac - bd
             // result = 10^2 * ac + 10^n/2 * adbc + bd
-            BigInteger a = new BigInteger(number1.toString().substring(0,half));
-            BigInteger b = new BigInteger(number1.toString().substring(half));
+            BigInteger a = new BigInteger(number1.toString().substring(0,number1.toString().length() - half));
+            BigInteger b = new BigInteger(number1.toString().substring(number1.toString().length() - half));
 
-            BigInteger c = new BigInteger(number2.toString().substring(0,half));
-            BigInteger d = new BigInteger(number2.toString().substring(half));
+            BigInteger c = new BigInteger(number2.toString().substring(0,number2.toString().length() - half));
+            BigInteger d = new BigInteger(number2.toString().substring(number2.toString().length() - half));
 
             BigInteger ac = karatsuba(a, c);
             BigInteger bd = karatsuba(b, d);
             BigInteger pq = karatsuba(a.add(b), c.add(d));
             BigInteger adbc = pq.subtract(ac).subtract(bd);
 
-            return new BigInteger(String.valueOf((int)Math.pow(10, half*2)))
-                    .multiply(ac)
-                    .add(new BigInteger(String.valueOf((int)Math.pow(10, half))).multiply(adbc))
-                    .add(bd);
+            return (ac.multiply(new BigInteger("10").pow(half*2))).add(adbc.multiply(new BigInteger("10").pow(half))).add(bd);
         }
     }
 }
