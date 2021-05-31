8c54f1403d2fcf8ba4c6e5d74e22b808d4020ad3
Andreii <andrei@omtia.com>
2021-05-31 07:00:25 +0200

    kosaraju

--- general/src/algorithms/Kosaraju.java
+++ general/src/algorithms/Kosaraju.java
@@ -10,13 +10,21 @@ public class Kosaraju {
     private static final int LIST_SIZE = 875_715;
     private static int currentLabel = LIST_SIZE-1;
     private static Map<Integer, Integer> scc = new HashMap<>(LIST_SIZE);
+    private static Map<Integer, Integer> scc_list = new HashMap<>(LIST_SIZE);
     private static List<Integer> topology = new ArrayList<>(LIST_SIZE);
+
+
+    // SCC_test_1 -> 3,3,3,0,0
+    // SCC_test_2 -> 3,3,2,0,0
+    // SCC_test_3 -> 3,3,1,1,0
+    // SCC_test_4 -> 7,1,0,0,0
+    // SCC_test_5 -> 6,3,2,1,0
 
     public static void main(String[] args) throws FileNotFoundException {
         Map<Integer, List<Integer>> adjList = new HashMap<>(LIST_SIZE);
         Map<Integer, List<Integer>> adjListReversed = new HashMap<>(LIST_SIZE);
 
-        Scanner in = new Scanner(new File("/home/act/mds/projects/git/algo/general/src/algorithms/SCC.txt"));
+        Scanner in = new Scanner(new File("/home/act/mds/projects/git/algo/general/src/algorithms/SCC_test_3.txt"));
 
         for(int i = 1; i <= LIST_SIZE; i++) {
             topology.add(-1);
@@ -44,11 +52,22 @@ public class Kosaraju {
 
         kosaraju(adjList, adjListReversed);
 
+        List<Integer> sccCount = scc.values().stream().sorted().collect(Collectors.toList());
+        Collections.reverse(sccCount);
+
+        System.out.println(sccCount);
+//        System.out.println(scc);
+//        System.out.println(scc_list);
+
+//        topology = topology.stream().sorted().distinct().collect(Collectors.toList());
+//        Collections.reverse(topology);
+//
+//        System.out.println(topology.toString());
     }
 
     public static void kosaraju(Map<Integer, List<Integer>> adjList, Map<Integer, List<Integer>> adjListReversed) {
-        Set<Integer> visited = new HashSet<>(875_715);
-        Set<Integer> visitedReversed = new HashSet<>(875_715);
+        Set<Integer> visited = new HashSet<>(LIST_SIZE);
+        Set<Integer> visitedReversed = new HashSet<>(LIST_SIZE);
 
         TopoSort(adjListReversed, visitedReversed);
 
@@ -63,33 +82,37 @@ public class Kosaraju {
 
     private static void DFS_SCC(Map<Integer, List<Integer>> adjList, int s, Set<Integer> visited) {
         visited.add(s);
+        scc_list.put(s, numSCC);
         int count = scc.getOrDefault(numSCC, 0);
         scc.put(numSCC, ++count);
 
         if(! /*NOT*/ adjList.containsKey(s)) return;
 
         for(Integer v : adjList.get(s)) {
-            if(! /*NOT*/ visited.contains(v)) {
-                DFS_SCC(adjList, v, visited);
-            }
+            //guard
+            if(visited.contains(v)) continue;
+
+            DFS_SCC(adjList, v, visited);
         }
      }
 
     private static void TopoSort(Map<Integer, List<Integer>> adjList, Set<Integer> visitedReversed) {
-        for(int v = 1; v < LIST_SIZE; v++) {
-            if(adjList.containsKey(v) && !/*NOT*/visitedReversed.contains(v)) {
-                DFS_Topo(adjList, v, visitedReversed);
-            }
+        for(Integer v : adjList.keySet()) {
+            // guard
+            if(visitedReversed.contains(v)) continue;
+
+            DFS_Topo(adjList, v, visitedReversed);
         }
     }
 
     private static void DFS_Topo(Map<Integer, List<Integer>> adjList, int s, Set<Integer> visitedReversed) {
         visitedReversed.add(s);
 
-        if(! /*NOT*/ adjList.containsKey(s)) return;
+        if(adjList.containsKey(s)) {
+            for(Integer v : adjList.get(s)) {
+                // guard
+                if(visitedReversed.contains(v)) continue;
 
-        for(Integer v : adjList.get(s)) {
-            if(! /*NOT*/ visitedReversed.contains(v)) {
                 DFS_Topo(adjList, v, visitedReversed);
             }
         }
