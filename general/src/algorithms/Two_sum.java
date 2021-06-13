81bca755ac2680f9a17ef067c55bb50e364c23e0
Andreii <andrei@omtia.com>
2021-06-13 22:30:47 +0200

    2sum

--- general/src/algorithms/Two_sum.java
+++ general/src/algorithms/Two_sum.java
@@ -1,37 +1,46 @@
 package algorithms;
 
 import java.io.File;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Two_sum {
+
+    private static final int RANGE_LEFT = -3;
+    private static final int RANGE_RIGHT = 10;
+
+
     public static void main(String[] args) throws Exception {
         // https://stackoverflow.com/questions/28951727/how-can-i-improve-my-2-sum-algorithm-for-a-range-of-numbers-using-a-hash-table
         Scanner in = new Scanner(new File("/home/act/mds/projects/git/algo/general/src/algorithms/2sum_test.txt"));
         Set<Long> set = new HashSet<>();
         Set<Long> sortedSet = new TreeSet<>();
 
+        Long number;
         while(in.hasNext()) {
-            set.add(in.nextLong());
+            number = in.nextLong();
+            set.add(number);
+            sortedSet.add(number);
         }
 
-        int answer = 0;
-        Long y;
-        int index = 0;
+        Long[] sortedArray = sortedSet.toArray(Long[]::new);
+        Map<Integer, Map<Integer, Boolean>> visited = new HashMap<>();
+        int answer = 0, i = 0, j = 0;
         for(Long x : set) {
-            for(Long t = Long.valueOf(3); t <= 10; t++) {
-                y = t - x;
-                if(x == y) continue;
-                if(set.contains(y)) {
-                    set.remove(x);
-                    set.remove(y);
-                    answer++;
-                };
+            int startIndex = Arrays.binarySearch(sortedArray, RANGE_LEFT - x),
+                endIndex = Arrays.binarySearch(sortedArray, RANGE_RIGHT - x), startInsertion, endInsertion;
+
+            if(startIndex < 0) {
+                startIndex = -startIndex -1;
             }
-            index++;
-            System.out.printf("At index: %d \n", index);
+
+            if(endIndex < 0) {
+                endIndex = -endIndex -1;
+            }
+
+            Long[] tArray = Arrays.copyOfRange(sortedArray, startIndex, endIndex);
+
+
+
         }
 
 //        for(int t = 3; t <= 10; t++) {
