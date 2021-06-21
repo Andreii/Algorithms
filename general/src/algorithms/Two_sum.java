c7b8b615f57204a52bf2b735a1e15f4f63c5ef79
Andreii <andrei@omtia.com>
2021-06-21 21:14:18 +0200

    2sum

--- general/src/algorithms/Two_sum.java
+++ general/src/algorithms/Two_sum.java
@@ -11,9 +11,10 @@ public class Two_sum {
 
     public static void main(String[] args) throws Exception {
         // https://stackoverflow.com/questions/28951727/how-can-i-improve-my-2-sum-algorithm-for-a-range-of-numbers-using-a-hash-table
-        Scanner in = new Scanner(new File("/home/act/mds/projects/git/algo/general/src/algorithms/2sum_test.txt"));
+        Scanner in = new Scanner(new File("/home/act/mds/projects/git/algo/general/src/algorithms/2sum.txt"));
         Set<Long> set = new HashSet<>();
         Set<Long> sortedSet = new TreeSet<>();
+        Set<Long> answerSet = new HashSet<>();
 
         Long number;
         while(in.hasNext()) {
@@ -25,9 +26,10 @@ public class Two_sum {
         Long[] sortedArray = sortedSet.toArray(Long[]::new);
         Map<Integer, Map<Integer, Boolean>> visited = new HashMap<>();
         int answer = 0, i = 0, j = 0;
+
         for(Long x : set) {
             int startIndex = Arrays.binarySearch(sortedArray, RANGE_LEFT - x),
-                endIndex = Arrays.binarySearch(sortedArray, RANGE_RIGHT - x), startInsertion, endInsertion;
+                endIndex = Arrays.binarySearch(sortedArray, RANGE_RIGHT - x);
 
             if(startIndex < 0) {
                 startIndex = -startIndex -1;
@@ -37,17 +39,11 @@ public class Two_sum {
                 endIndex = -endIndex -1;
             }
 
-            Long[] tArray = Arrays.copyOfRange(sortedArray, startIndex, endIndex);
-
+            for(Long t : Arrays.copyOfRange(sortedArray, startIndex, endIndex)) {
+                answerSet.add(t);
+            }
         }
 
-//        for(int t = 3; t <= 10; t++) {
-//            for(Long x : set) {
-//                if(t-x == x) continue;
-//                if (set.contains(t-x)) { answer++;};
-//            }
-//        }
-
-        System.out.printf("Answer is the following: %d", answer);
+        System.out.printf("Answer is the following: %d", answerSet.size());
     }
 }
