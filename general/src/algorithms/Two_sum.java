706bfe42a46b436a58640d9647601754cdb03cde
Andreii <andrei@omtia.com>
2021-06-22 09:51:48 +0200

    2sum

--- general/src/algorithms/Two_sum.java
+++ general/src/algorithms/Two_sum.java
@@ -5,14 +5,13 @@ import java.util.*;
 
 public class Two_sum {
 
-    private static final int RANGE_LEFT = 3;
-    private static final int RANGE_RIGHT = 4;
-
+    private static final int RANGE_LEFT = 1;
+    private static final int RANGE_RIGHT = 10;
 
     public static void main(String[] args) throws Exception {
         // https://stackoverflow.com/questions/28951727/how-can-i-improve-my-2-sum-algorithm-for-a-range-of-numbers-using-a-hash-table
-//        Scanner in = new Scanner(new File("/home/act/mds/projects/git/algo/general/src/algorithms/2sum.txt"));
-        Scanner in = new Scanner(new File("/Users/andrei/IdeaProjects/algo/general/src/algorithms/2sum_test4.txt"));
+        Scanner in = new Scanner(new File("/home/act/mds/projects/git/algo/general/src/algorithms/2sum_test3.txt"));
+//        Scanner in = new Scanner(new File("/Users/andrei/IdeaProjects/algo/general/src/algorithms/2sum_test4.txt"));
         // 2sum_test -> 3,10 -> 8
         // 2sum_test2 -> 10,40 -> 11
         // 2sum_test3 -> 1,10 -> 2
@@ -52,6 +51,21 @@ public class Two_sum {
             }
         }
 
-        System.out.printf("Answer is the following: %d", answerSet.size());
+        // brute force
+//        int answer = 0;
+//        Set<Long> answerBruteForce = new HashSet<>();
+//        for(int x = 0; x < sortedArray.length; x++) {
+//            for (int y = x; y < sortedArray.length; y++) {
+//                Long t = sortedArray[x] + sortedArray[y];
+//                if(answerBruteForce.contains(t)) continue;
+//                if( t >= RANGE_LEFT && t <= RANGE_RIGHT) {
+//                    answerBruteForce.add(t);
+//                    answer++;
+//                }
+//            }
+//        }
+
+        System.out.printf("Answer is the following: %d\n", answerSet.size());
+//        System.out.printf("Answer is the following: %d", answer);
     }
 }
