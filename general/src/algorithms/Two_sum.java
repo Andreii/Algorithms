99ba253351b94881f8ced1c6499a1859948c0dd4
Andreii <andrei@omtia.com>
2021-06-22 06:52:33 +0200

    2sum

--- general/src/algorithms/Two_sum.java
+++ general/src/algorithms/Two_sum.java
@@ -5,14 +5,17 @@ import java.util.*;
 
 public class Two_sum {
 
-    private static final int RANGE_LEFT = -10000;
-    private static final int RANGE_RIGHT = 10000;
+    private static final int RANGE_LEFT = 1;
+    private static final int RANGE_RIGHT = 10;
 
 
     public static void main(String[] args) throws Exception {
         // https://stackoverflow.com/questions/28951727/how-can-i-improve-my-2-sum-algorithm-for-a-range-of-numbers-using-a-hash-table
 //        Scanner in = new Scanner(new File("/home/act/mds/projects/git/algo/general/src/algorithms/2sum.txt"));
-        Scanner in = new Scanner(new File("/Users/andrei/IdeaProjects/algo/general/src/algorithms/2sum.txt"));
+        Scanner in = new Scanner(new File("/Users/andrei/IdeaProjects/algo/general/src/algorithms/2sum_test3.txt"));
+        // 2sum_test -> 3,10 -> 8
+        // 2sum_test2 -> 10,40 -> 11
+        // 2sum_test3 -> 1,10 -> 2
         Set<Long> set = new HashSet<>();
         Set<Long> sortedSet = new TreeSet<>();
         Set<Long> answerSet = new HashSet<>();
@@ -25,7 +28,7 @@ public class Two_sum {
         }
 
         Long[] sortedArray = sortedSet.toArray(Long[]::new);
-        for(Long x : set) {
+        for(Long x : sortedSet) {
             int startIndex = Arrays.binarySearch(sortedArray, RANGE_LEFT - x),
                 endIndex = Arrays.binarySearch(sortedArray, RANGE_RIGHT - x);
 
