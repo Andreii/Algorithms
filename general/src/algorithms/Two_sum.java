11ba7f645fff9a7e8d4ee334afb6d588d9730113
Andreii <andrei@omtia.com>
2021-06-24 20:18:25 +0200

    2sum

--- general/src/algorithms/Two_sum.java
+++ general/src/algorithms/Two_sum.java
@@ -5,12 +5,12 @@ import java.util.*;
 
 public class Two_sum {
 
-    private static final int RANGE_LEFT = 1;
-    private static final int RANGE_RIGHT = 10;
+    private static final int RANGE_LEFT = -10_000;
+    private static final int RANGE_RIGHT = 10_000;
 
     public static void main(String[] args) throws Exception {
         // https://stackoverflow.com/questions/28951727/how-can-i-improve-my-2-sum-algorithm-for-a-range-of-numbers-using-a-hash-table
-        Scanner in = new Scanner(new File("/home/act/mds/projects/git/algo/general/src/algorithms/2sum_test3.txt"));
+        Scanner in = new Scanner(new File("/home/act/mds/projects/git/algo/general/src/algorithms/2sum.txt"));
 //        Scanner in = new Scanner(new File("/Users/andrei/IdeaProjects/algo/general/src/algorithms/2sum_test4.txt"));
         // 2sum_test -> 3,10 -> 8
         // 2sum_test2 -> 10,40 -> 11
@@ -19,19 +19,20 @@ public class Two_sum {
         // 2sum_test4 -> 3,4 -> 2
         Set<Long> set = new HashSet<>();
         Set<Long> sortedSet = new TreeSet<>();
+        List<Long> sortedList = new ArrayList<>();
         Set<Long> answerSet = new HashSet<>();
 
         Long number;
         while(in.hasNext()) {
             number = in.nextLong();
             set.add(number);
-            sortedSet.add(number);
+            sortedList.add(number);
         }
 
-        Long[] sortedArray = sortedSet.toArray(Long[]::new);
-        for(Long x : sortedSet) {
-            int startIndex = Arrays.binarySearch(sortedArray, RANGE_LEFT - x),
-                endIndex = Arrays.binarySearch(sortedArray, RANGE_RIGHT - x);
+        Collections.sort(sortedList);
+        for(Long x : sortedList) {
+            int startIndex = Collections.binarySearch(sortedList, RANGE_LEFT - x),
+                endIndex = Collections.binarySearch(sortedList, RANGE_RIGHT - x);
 
             if(startIndex < 0) {
                 startIndex = -startIndex ;
@@ -41,10 +42,10 @@ public class Two_sum {
                 endIndex = -endIndex ;
             }
 
-            endIndex = Math.min(endIndex, sortedArray.length -1);
+            endIndex = Math.min(endIndex, sortedList.size() -1);
 
             for(int i = startIndex; i <= endIndex; i++) {
-                Long y = sortedArray[i];
+                Long y = sortedList.get(i);
                 Long t = x+y;
                 if(answerSet.contains(t)) continue;
                 if (!x.equals(y) && t >= RANGE_LEFT && t <= RANGE_RIGHT ) answerSet.add(x+y);
@@ -65,7 +66,15 @@ public class Two_sum {
 //            }
 //        }
 
+        Long[] answerArr = answerSet.toArray(Long[]::new);
+        Arrays.sort(answerArr);
+
         System.out.printf("Answer is the following: %d\n", answerSet.size());
+        System.out.print("[");
+        for(Long a : answerArr) {
+            System.out.printf("%d, ", a);
+        }
+        System.out.print("]");
 //        System.out.printf("Answer is the following: %d", answer);
     }
 }
