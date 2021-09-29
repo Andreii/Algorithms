b0dad1dea580a17e01a11d809051c8ed914c5c0a
Andreii <andrei@omtia.com>
2021-09-29 21:25:15 +0200

    2sum

--- general/src/algorithms/WeightedSum.java
+++ general/src/algorithms/WeightedSum.java
@@ -2,10 +2,7 @@ package algorithms;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class WeightedSum {
     public static void main(String[] args) throws FileNotFoundException {
@@ -13,19 +10,36 @@ public class WeightedSum {
 
         Scanner scanner = new Scanner(file);
 
-        String count = scanner.nextLine();
+        Integer count = Integer.valueOf(scanner.nextLine());
 
-        TreeMap<Integer, Integer> job_dif = new TreeMap<>();
+        TreeMap<Integer, SortedSet<Integer>> job_dif = new TreeMap<>(Collections.reverseOrder());
+
         List<List<Integer>> job_diff = new ArrayList<>();
 
         while(scanner.hasNext()) {
             String[] props = scanner.nextLine().split(" ");
             Integer job_weight = Integer.parseInt(props[0]);
             Integer job_length = Integer.parseInt(props[1]);
+            Integer diff = job_weight - job_length;
 
-            job_dif.put(job_weight - job_length, job_weight);
-
-            System.out.println();
+            if(job_dif.containsKey(diff)) {
+                SortedSet<Integer> value = job_dif.get(diff);
+                value.add(job_weight);
+                job_dif.put(diff, value);
+            } else {
+                job_dif.put(diff, new TreeSet<>(Collections.reverseOrder()) {{ add( job_weight); }});
+            }
         }
+
+        Long result = 0L;
+
+        for(Map.Entry<Integer, SortedSet<Integer>> entry : job_dif.entrySet()) {
+
+            for(Integer v : entry.getValue()) {
+                result += entry.getKey() * v;
+            }
+        }
+
+        System.out.println("Result is: " + result);
     }
 }
