391c465243ab434685bec8467143e14dad39c420
Andreii <andrei@omtia.com>
2022-01-23 17:15:24 +0100

    leetcode

---
+++ leetcode/src/solutions/_121_Best_time_to_buy_and_sell_stock.java
@@ -0,0 +1,19 @@
+package solutions;
+
+public class _121_Best_time_to_buy_and_sell_stock {
+
+}
+
+class Solution {
+    // Kadane
+    public int maxProfit(int[] prices) {
+        int min = prices[0], max = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i] - min);
+        }
+
+        return max;
+    }
+}
