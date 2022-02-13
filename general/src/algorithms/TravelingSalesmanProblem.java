a1423a24e1820d68d3acab03ccf1a7b7d1cf0424
Andreii <andrei@omtia.com>
2022-02-13 17:38:33 +0100

    Traveling Salesman Problem

---
+++ general/src/algorithms/TravelingSalesmanProblem.java
@@ -0,0 +1,15 @@
+package algorithms;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class TravelingSalesmanProblem {
+    class Coord {
+        float x,y;
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(new File("general/resources/tsp.txt"));
+
+        int count = Integer.parseInt(scanner.nextLine());
+    }
+}
