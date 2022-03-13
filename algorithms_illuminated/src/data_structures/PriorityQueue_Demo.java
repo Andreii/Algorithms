89edb6b23a80cd9088afefc973ac2f3199522247
Andreii <andrei@omtia.com>
2022-03-13 13:09:09 +0100

    1011 leetcode

--- algorithms_illuminated/src/data_structures/PriorityQueue_Demo.java
+++ algorithms_illuminated/src/data_structures/PriorityQueue_Demo.java
@@ -4,11 +4,30 @@ import java.util.*;
 
 public class PriorityQueue_Demo {
     public static void main(String[] args) {
-        Integer[] numbers = new Integer[] {2, 4, 7 ,3, 1, 8};
+        Integer[] numbers = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         PriorityQueue<Integer> min = new PriorityQueue<>() {{ addAll(Arrays.asList(numbers)); }};
 
         System.out.println(min.poll());
         System.out.println(min.poll());
         System.out.println(min.poll());
+    }
+
+    public static boolean isFeasable(int[] weights, int maxWeight, int d) {
+        LinkedList<Integer> q = new LinkedList<>();
+        for(int w : weights) {
+            q.add(w);
+        }
+
+        int sum = 0;
+        while( q.size() > 0 && d >= 0 ) {
+            while(q.size() > 0 && sum + q.peek() <= maxWeight) {
+                sum += q.poll();
+            }
+
+            sum = 0;
+            d--;
+        }
+
+        return q.size() == 0;
     }
 }
---
+++ leetcode/src/solutions/_1011_Capacity_to_ship_packages_within_d_days.java
@@ -0,0 +1,102 @@
+package solutions;
+
+import java.util.LinkedList;
+
+/**
+ * A conveyor belt has packages that must be shipped from one port to another within days days.
+ *
+ * The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the
+ * conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the
+ * ship.
+ *
+ * Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped
+ * within days days.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+ * Output: 15
+ * Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
+ * 1st day: 1, 2, 3, 4, 5
+ * 2nd day: 6, 7
+ * 3rd day: 8
+ * 4th day: 9
+ * 5th day: 10
+ *
+ * Note that the cargo must be shipped in the order given, so using a ship of capacity 14 and splitting the packages
+ * into parts like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) is not allowed.
+ * Example 2:
+ *
+ * Input: weights = [3,2,2,4,1,4], days = 3
+ * Output: 6
+ * Explanation: A ship capacity of 6 is the minimum to ship all the packages in 3 days like this:
+ * 1st day: 3, 2
+ * 2nd day: 2, 4
+ * 3rd day: 1, 4
+ * Example 3:
+ *
+ * Input: weights = [1,2,3,1,1], days = 4
+ * Output: 3
+ * Explanation:
+ * 1st day: 1
+ * 2nd day: 2
+ * 3rd day: 3
+ * 4th day: 1, 1
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= days <= weights.length <= 5 * 104
+ * 1 <= weights[i] <= 500
+ */
+public class _1011_Capacity_to_ship_packages_within_d_days {
+    public boolean isFeasable(int[] weights, int maxWeight, int d) {
+        LinkedList<Integer> q = new LinkedList<>();
+        for(int w : weights) {
+            q.add(w);
+        }
+
+        // 2 4 1 4
+        int sum = 0; // 0
+        // q = 6
+        // d = 2
+        while( q.size() > 0 && d >= 1 ) {
+            while(q.size() > 0 && sum + q.peek() <= maxWeight) {
+                sum += q.poll();
+            }
+
+            sum = 0;
+            d--;
+        }
+
+        return q.size() == 0;
+    }
+
+    public int shipWithinDays(int[] weights, int days) {
+
+        // 1,2,3,4,5,6,7
+        int sumW = 0;
+        int maxW = 0;
+        int d = days;
+        for(int w : weights) {
+            sumW += w;
+            maxW = Math.max(maxW, w);
+        }
+
+        int L = maxW, R = sumW;
+        int ans = -1;
+        while(L <= R ) {
+            int mid = L + (R-L)/2;
+            if(isFeasable(weights, mid, d)) {
+                ans = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
