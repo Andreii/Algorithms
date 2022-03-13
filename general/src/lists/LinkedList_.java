37064984f75a612de033414f0c1d748cff19bf82
Andreii <andrei@omtia.com>
2022-03-13 19:03:51 +0100

    LinkedList experimenting

---
+++ general/src/lists/LinkedList_.java
@@ -0,0 +1,47 @@
+package lists;
+
+import java.util.LinkedList;
+
+public class LinkedList_ {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        // add to end
+        // add
+        // offer
+        // addLast
+        list.offer(3);
+        list.add(4);
+        list.offer(5);
+        list.add(6);
+        list.addLast(7);
+
+        // add to start
+        // push
+        list.addFirst(0);
+        list.push(1);
+        list.push(2);
+
+        // remove from end
+        list.removeLast();
+        list.pollLast();
+
+        // remove from start
+        list.removeFirst(); // exception
+        list.remove(); // exception
+        list.pop(); // exception
+        list.poll(); // NO exception
+
+        // peek
+        // check first
+        list.peek();
+        list.peekFirst();
+        // check last
+        list.peekLast();
+
+        System.out.println("List with offer: ");
+        for(Integer e : list) {
+            System.out.print(e + ",");
+        }
+    }
+}
--- leetcode/src/solutions/experiment.java
+++ leetcode/src/solutions/experiment.java
@@ -1,33 +1,9 @@
 package solutions;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class experiment {
     public static void main(String[] args) {
-        int[] test = new int[] {2,7,11,26};
-        System.out.println(solve2(test));
-    }
-    public static boolean solve(int[] nums) {
-        int n = nums.length;
 
-        for(int i = 0; i < n-1; i++) {
-            for(int j = i+1; j < n; j++) {
-                if(nums[i] == nums[j]) return true;
-            }
-        }
-        return false;
-    }
-    public static boolean solve2(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums) {
-            if(!set.add(num)){
-                return true;
-            }
-        }
-        return false;
     }
 }
