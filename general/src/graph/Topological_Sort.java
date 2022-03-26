f31262bbf945fc29f6dc09c8513cfe15e301babf
Andreii <andrei@omtia.com>
2022-03-26 08:30:26 +0100

    Topo kahn bfs sort

--- general/src/graph/Topological_Sort.java
+++ general/src/graph/Topological_Sort.java
@@ -1,10 +1,7 @@
 package graph;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  *        2  ->  4
@@ -15,11 +12,11 @@ import java.util.Scanner;
  *      . /      /
  *       6   -> 7
  *
- *       topo: 1 6 2 3 4 7 5
+ *       (topo.txt): 1 6 2 3 4 7 5
  */
 public class Topological_Sort {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(new File("general/resources/topo2.txt"));
+        Scanner scanner = new Scanner(new File("general/resources/topo.txt"));
 
         int n =  scanner.nextInt();
         List<List<Integer>> adj = new ArrayList<>(n);
@@ -40,8 +37,11 @@ public class Topological_Sort {
         for(int i = 1; i <= n; i++)
             if(!visited[i])
                 topoSortDFS(i, result, visited, adj);
-
         System.out.println("Topo DFS sort: " + result);
+
+        LinkedList<Integer> res = new LinkedList<>();
+        topoSortKahnBFS(res,adj,n);
+        System.out.println("Topo Kahn sort: " + res);
     }
 
     public static void topoSortDFS(int i, LinkedList<Integer> result, boolean[] visited, List<List<Integer>> adj) {
@@ -52,5 +52,33 @@ public class Topological_Sort {
             }
         }
         result.addFirst(i);
+    }
+
+    public static void topoSortKahnBFS(LinkedList<Integer> result, List<List<Integer>> adj, int n) {
+        int[] indegree = new int[n+1];
+
+        Arrays.fill(indegree, 0);
+        for(int i = 1; i < adj.size(); i++) {
+            List<Integer> dests = adj.get(i);
+            for(Integer dest : dests) {
+                indegree[dest]++;
+            }
+        }
+
+        LinkedList<Integer> q = new LinkedList<>();
+        for(int i = 1; i < indegree.length; i++) {
+            if(indegree[i] == 0) q.add(i);
+        }
+
+        while(!q.isEmpty()) {
+            int u = q.poll();
+
+            result.add(u);
+            for(Integer node : adj.get(u)) {
+                if(--indegree[node] == 0) {
+                    q.add(node);
+                }
+            }
+        }
     }
 }
